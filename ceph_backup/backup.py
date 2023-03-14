b0ad6df132d296ff9f2f634e82e747ed01ce0eb1
Remi Rampin <remi@rampin.org>
2023-03-14 00:10:42 -0400

    Refactor, add metrics

--- ceph_backup/backup.py
+++ ceph_backup/backup.py
@@ -8,32 +8,11 @@ import os
 import shlex
 import subprocess
 
+from .metadata import METADATA_PREFIX, ANNOTATION_LAST_ATTEMPT, NAMESPACE, \
+    parse_date, list_volumes_to_backup
+
 
 logger = logging.getLogger(__name__)
-
-
-METADATA_PREFIX = 'cephbackup.hpc.nyu.edu/'
-
-ANNOTATION_ENABLED = METADATA_PREFIX + 'backup'
-ANNOTATION_LAST_ATTEMPT = METADATA_PREFIX + 'last-start'
-
-NAMESPACE = 'ceph-backup'
-
-
-def parse_bool(value):
-    if value is None:
-        return None
-    elif value.lower() in ('1', 'yes', 'true'):
-        return True
-    elif value.lower() in ('0', 'no', 'false'):
-        return False
-    else:
-        return None
-
-
-def parse_date(s):
-    assert len(s) == 20 and s[-1] == 'Z'
-    return datetime.fromisoformat(s[:-1])
 
 
 def render_date(dt):
@@ -74,21 +53,6 @@ def format_env(**kwargs):
         else:
             assert False, v
     return result
-
-
-def list_namespaces(api):
-    corev1 = k8s_client.CoreV1Api(api)
-    return corev1.list_namespace().items
-
-
-def list_persistent_volume_claims(api):
-    corev1 = k8s_client.CoreV1Api(api)
-    return corev1.list_persistent_volume_claim_for_all_namespaces().items
-
-
-def list_persistent_volumes(api):
-    corev1 = k8s_client.CoreV1Api(api)
-    return corev1.list_persistent_volume().items
 
 
 def main():
@@ -137,92 +101,7 @@ def main():
 
 
 def build_list_to_backup(api, now):
-    # List all namespaces, collect backup configuration
-    namespaces = {}
-    for ns in list_namespaces(api):
-        annotations = ns.metadata.annotations or {}
-        namespaces[ns.metadata.name] = {
-            'backup': parse_bool(annotations.get(ANNOTATION_ENABLED)),
-        }
-    logger.info("Got %d namespaces", len(namespaces))
-
-    # List all PVCs, collect configuration and PV links
-    claims = {}
-    for pvc in list_persistent_volume_claims(api):
-        annotations = pvc.metadata.annotations or {}
-        claims[pvc.spec.volume_name] = {
-            'backup': parse_bool(annotations.get(ANNOTATION_ENABLED)),
-            'namespace': pvc.metadata.namespace,
-            'name': pvc.metadata.name,
-        }
-    logger.info("Got %d claims", len(claims))
-
-    # List all PVs, collect configuration
-    volumes = []
-    for pv in list_persistent_volumes(api):
-        annotations = pv.metadata.annotations or {}
-        last_backup = annotations.get(ANNOTATION_LAST_ATTEMPT)
-        if last_backup:
-            last_backup = parse_date(last_backup)
-        if pv.spec.csi and pv.spec.csi.driver == 'rbd.csi.ceph.com':
-            vol = {
-                'name': pv.metadata.name,
-                'backup': parse_bool(annotations.get(ANNOTATION_ENABLED)),
-                'last_backup': last_backup,
-                'mode': pv.spec.volume_mode,
-                'size': pv.spec.capacity.get('storage'),
-                'rbd_pool': pv.spec.csi.volume_attributes['pool'],
-                'rbd_name': pv.spec.csi.volume_attributes['imageName'],
-                'csi': {
-                    'cluster_id': pv.spec.csi.volume_attributes['clusterID'],
-                }
-            }
-            if pv.spec.csi.node_stage_secret_ref:
-                vol['csi']['secret'] = (
-                    pv.spec.csi.node_stage_secret_ref.namespace,
-                    pv.spec.csi.node_stage_secret_ref.name,
-                )
-            if pv.spec.csi.fs_type:
-                vol['csi']['fstype'] = pv.spec.csi.fs_type
-            volumes.append(vol)
-    logger.info("Got %d volumes", len(volumes))
-
-    # Build list of RBD volumes to backup
-    to_backup = []
-    for pv in volumes:
-        try:
-            claim = claims[pv['name']]
-        except KeyError:
-            logger.warning(
-                "PersistentVolume without a PersistentVolumeClaim: %s",
-                pv['name'],
-            )
-            continue
-        ns = namespaces[claim['namespace']]
-
-        if claim['namespace'] == NAMESPACE:
-            continue
-
-        # Check configuration
-        if pv['backup'] is False:
-            continue
-        elif pv['backup'] is None:
-            if ns['backup'] is False:
-                continue
-            if claim['backup'] is False:
-                continue
-
-        to_backup.append({
-            'pv': pv['name'],
-            'mode': pv['mode'],
-            'namespace': claim['namespace'],
-            'name': claim['name'],
-            'last_backup': pv['last_backup'],
-            'rbd_pool': pv['rbd_pool'],
-            'rbd_name': pv['rbd_name'],
-            'csi': pv['csi'],
-            'size': pv['size'],
-        })
+    to_backup = list_volumes_to_backup(api)
 
     # Select based on last attempt
     limit = 24 * 3600 - 30 * 60  # 23:30:00
--- ceph_backup/metrics.py
+++ ceph_backup/metrics.py
@@ -1,3 +1,124 @@
+import argparse
+from datetime import datetime
+import kubernetes.client as k8s_client
+import kubernetes.config as k8s_config
+import logging
+import math
+from prometheus_client import REGISTRY, make_wsgi_app
+from prometheus_client.exposition import ThreadingWSGIServer
+from prometheus_client.metrics_core import GaugeMetricFamily, \
+    HistogramMetricFamily
+from wsgiref.simple_server import make_server, WSGIRequestHandler
+
+from .metadata import METADATA_PREFIX, NAMESPACE, list_volumes_to_backup
+
+
+logger = logging.getLogger(__name__)
+
+
+class Collector(object):
+    def collect(self):
+        now = datetime.utcnow()
+
+        with k8s_client.ApiClient() as api:
+            to_backup = list_volumes_to_backup(api)
+
+            batchv1 = k8s_client.BatchV1Api(api)
+            jobs = batchv1.list_namespaced_job(
+                NAMESPACE,
+                label_selector=METADATA_PREFIX + 'volume-type=rbd',
+            ).items
+
+        volumes_backed_up = GaugeMetricFamily(
+            'volumes_backed_up',
+            "Volumes that have backups enabled",
+            labels=['namespace'],
+        )
+        volume_backup_due = HistogramMetricFamily(
+            'volume_backups_due',
+            "Volume backups by due date (in hours)",
+            labels=['namespace'],
+        )
+        running_rbd_backup_jobs = GaugeMetricFamily(
+            'running_backup_jobs',
+            "Number of backup jobs running now",
+            labels=['namespace'],
+        )
+
+        namespaces = {}
+        for vol in to_backup:
+            try:
+                data = namespaces[vol['namespace']]
+            except KeyError:
+                data = {'volumes': 0, 'due': [0] * 25}
+                namespaces[vol['namespace']] = data
+
+            data['volumes'] += 1
+
+            if vol['last_backup'] is None:
+                due = 0
+            else:
+                due = (vol['last_backup'] - now).total_seconds() + 24 * 3600
+                due = max(0, due)
+                due = math.floor(due / 3600)
+                due = min(24, due)
+            data['due'][due] += 1
+
+        for namespace, data in namespaces.items():
+            volumes_backed_up.add_metric([namespace], data['volumes'])
+            sum_value = 0
+            buckets = []
+            for due, value in enumerate(data['due'][:24]):
+                buckets.append((str(due), value))
+                sum_value += value
+            buckets.append(('+Inf', data['due'][24]))
+            sum_value += data['due'][24]
+            volume_backup_due.add_metric([namespace], buckets, sum_value)
+
+        running_jobs = {}
+        for job in jobs:
+            if not job.status.completion_time:
+                ns = job.metadata.annotations[METADATA_PREFIX + 'start-time']
+                running_jobs[ns] = running_jobs.get(ns, 0) + 1
+
+        for namespace, value in running_jobs.items():
+            running_rbd_backup_jobs.add_metric([namespace], value)
+
+        return [volumes_backed_up, volume_backup_due, running_rbd_backup_jobs]
+
+
+class SilentHandler(WSGIRequestHandler):
+    """WSGI handler that does not log requests."""
+
+    def log_message(self, format, *args):
+        """Log nothing."""
+
+
 def main():
-    # TODO: Read annotations on PVs, expose metrics
-    pass
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+    )
+
+    parser = argparse.ArgumentParser(
+        'ceph-backup-metrics',
+        description="Expose metrics from ceph-backup operations",
+    )
+    parser.add_argument('--kubeconfig', nargs=1)
+    args = parser.parse_args()
+
+    if args.kubeconfig:
+        logger.info("Using specified config file")
+        k8s_config.load_kube_config(args.kubeconfig[0])
+    else:
+        logger.info("Using in-cluster config")
+        k8s_config.load_incluster_config()
+
+    REGISTRY.register(Collector())
+
+    httpd = make_server(
+        '0.0.0.0', 8080,
+        make_wsgi_app(),
+        ThreadingWSGIServer, handler_class=SilentHandler,
+    )
+    httpd.serve_forever()
