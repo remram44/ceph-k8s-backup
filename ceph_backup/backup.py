1deb7b9acd5d08ba86074c14be4cc4c27e6827fe
Remi Rampin <remi@rampin.org>
2023-03-27 18:05:38 -0400

    Implement block device backup

--- ceph_backup/backup.py
+++ ceph_backup/backup.py
@@ -44,6 +44,17 @@ def check_call(args):
         raise subprocess.CalledProcessError(retcode, args)
 
 
+def check_output(args):
+    logger.info("> %s", ' '.join(shlex.quote(a) for a in args))
+    with subprocess.Popen(args, stdout=subprocess.PIPE) as process:
+        stdout, stderr = process.communicate()
+        retcode = process.poll()
+    logger.info("-> %d", retcode)
+    if retcode != 0:
+        raise subprocess.CalledProcessError(retcode, args)
+    return stdout
+
+
 def format_env(**kwargs):
     result = []
     for k, v in kwargs.items():
@@ -249,7 +260,7 @@ def cleanup_jobs(api):
                     },
                 )
 
-        # Clean the snapshot and cloned image
+        # Remove the snapshot and cloned image
         rbd_pool = meta.labels[METADATA_PREFIX + 'rbd-pool']
         rbd_name = meta.labels[METADATA_PREFIX + 'rbd-name']
         rbd_fq_backup_img = rbd_pool + '/' + 'backup-' + rbd_name
@@ -259,6 +270,21 @@ def cleanup_jobs(api):
         if call(['rbd', 'info', rbd_fq_snapshot]) == 0:
             call(['rbd', 'snap', 'unprotect', rbd_fq_snapshot])
             check_call(['rbd', 'snap', 'rm', rbd_fq_snapshot])
+
+        # Remove the PV, PVC, and CM if any
+        pv = meta.labels[METADATA_PREFIX + 'pv-name']
+        label_selector = METADATA_PREFIX + 'pv-name=%s' % pv
+        corev1.delete_collection_persistent_volume(
+            label_selector=label_selector,
+        )
+        corev1.delete_collection_namespaced_persistent_volume_claim(
+            NAMESPACE,
+            label_selector=label_selector,
+        )
+        corev1.delete_collection_namespaced_config_map(
+            NAMESPACE,
+            label_selector=label_selector,
+        )
 
         # Annotate job
         batchv1.patch_namespaced_job(
@@ -379,4 +405,176 @@ def backup_rbd_fs(api, ceph, vol, now):
 
 
 def backup_rbd_block(api, ceph, vol, now):
-    logger.warning("Unsupported volume mode %r", vol['mode'])
+    corev1 = k8s_client.CoreV1Api(api)
+    batchv1 = k8s_client.BatchV1Api(api)
+
+    rbd_fq_image = vol['rbd_pool'] + '/' + vol['rbd_name']
+    rbd_fq_snapshot = rbd_fq_image + '@backup'
+    rbd_backup_img = 'backup-' + vol['rbd_name']
+    rbd_fq_backup_img = vol['rbd_pool'] + '/' + rbd_backup_img
+
+    # Clean old snapshots and cloned images for this image
+    if call(['rbd', 'info', rbd_fq_backup_img]) == 0:
+        check_call(['rbd', 'rm', rbd_fq_backup_img])
+    if call(['rbd', 'info', rbd_fq_snapshot]) == 0:
+        call(['rbd', 'snap', 'unprotect', rbd_fq_snapshot])
+        check_call(['rbd', 'snap', 'rm', rbd_fq_snapshot])
+
+    # Make a snapshot
+    check_call(['rbd', 'snap', 'create', rbd_fq_snapshot])
+
+    # Turn it into an image
+    check_call(['rbd', 'snap', 'protect', rbd_fq_snapshot])
+    check_call(['rbd', 'clone', rbd_fq_snapshot, rbd_fq_backup_img])
+
+    # Get layout
+    layout_json = check_output([
+        'rbd', 'diff', '--whole-object', '--format=json', rbd_fq_image,
+    ]).decode('utf-8')
+
+    labels = {
+        METADATA_PREFIX + 'volume-type': 'rbd',
+        METADATA_PREFIX + 'volume-mode': 'block',
+        METADATA_PREFIX + 'pv-name': vol['pv'],
+        METADATA_PREFIX + 'pvc-namespace': vol['namespace'],
+        METADATA_PREFIX + 'pvc-name': vol['name'],
+        METADATA_PREFIX + 'rbd-pool': vol['rbd_pool'],
+        METADATA_PREFIX + 'rbd-name': vol['rbd_name'],
+    }
+
+    # Create a PersistentVolume
+    pv = corev1.create_persistent_volume(k8s_client.V1PersistentVolume(
+        metadata=k8s_client.V1ObjectMeta(
+            name='backup-rbd-block-%s' % vol['pv'],
+            labels=labels,
+        ),
+        spec=k8s_client.V1PersistentVolumeSpec(
+            access_modes=['ReadWriteMany'],
+            capacity={'storage': vol['size']},
+            persistent_volume_reclaim_policy='Delete',
+            storage_class_name='ceph-backup',
+            volume_mode='Block',
+            rbd=k8s_client.V1RBDVolumeSource(
+                monitors=ceph['monitors'],
+                pool=vol['rbd_pool'],
+                image=rbd_backup_img,
+                secret_ref=k8s_client.V1SecretReference(
+                    name=ceph['secret'],
+                    namespace=NAMESPACE,
+                ),
+                user=ceph['user'],
+            ),
+        ),
+    ))
+    logger.info("Created PersistentVolume %s", pv.metadata.name)
+
+    # Create a PersistentVolumeClaim
+    pvc = corev1.create_namespaced_persistent_volume_claim(
+        NAMESPACE,
+        k8s_client.V1PersistentVolumeClaim(
+            metadata=k8s_client.V1ObjectMeta(
+                name='backup-rbd-block-%s' % vol['pv'],
+                labels=labels,
+            ),
+            spec=k8s_client.V1PersistentVolumeClaimSpec(
+                access_modes=['ReadWriteMany'],
+                resources=k8s_client.V1ResourceRequirements(
+                    requests={'storage': vol['size']},
+                ),
+                storage_class_name='ceph-backup',
+                volume_mode='Block',
+                volume_name=pv.metadata.name,
+            ),
+        ),
+    )
+    logger.info("Created PersistentVolumeClaim %s", pvc.metadata.name)
+
+    # Create a configmap to store the layout
+    cm = corev1.create_namespaced_config_map(
+        NAMESPACE,
+        k8s_client.V1ConfigMap(
+            metadata=k8s_client.V1ObjectMeta(
+                name='backup-rbd-block-%s' % vol['pv'],
+                labels=labels,
+            ),
+            data={'layout.json': layout_json},
+        ),
+    )
+    logger.info("Created ConfigMap %s", cm.metadata.name)
+
+    # Create a job to do the backup
+    script = (
+        'streaming-qcow2-writer /disk /layout/layout.json'
+        + ' | restic'
+        + ' -r $(URL)'
+        + ' --host $(HOST)'
+        + ' backup --stdin --stdin-filename disk.qcow2'
+    )
+    job = batchv1.create_namespaced_job(NAMESPACE, k8s_client.V1Job(
+        metadata=k8s_client.V1ObjectMeta(
+            generate_name='backup-rbd-block-%s-' % vol['namespace'],
+            labels=labels,
+            annotations={
+                METADATA_PREFIX + 'start-time': render_date(now),
+            },
+        ),
+        spec=k8s_client.V1JobSpec(
+            active_deadline_seconds=12 * 3600,
+            ttl_seconds_after_finished=23 * 3600,
+            template=k8s_client.V1PodTemplateSpec(
+                metadata=k8s_client.V1ObjectMeta(
+                    labels=labels,
+                ),
+                spec=k8s_client.V1PodSpec(
+                    restart_policy='Never',
+                    containers=[
+                        k8s_client.V1Container(
+                            name='backup',
+                            image=BACKUP_IMAGE,
+                            args=['sh', '-c', script],
+                            env=format_env(
+                                URL=('secret', RESTIC_SECRET_NAME, 'url'),
+                                HOST='rbd-block-%s-nspvc-%s' % (
+                                    vol['namespace'],
+                                    vol['name'],
+                                ),
+                                RESTIC_PASSWORD=(
+                                    'secret', RESTIC_SECRET_NAME, 'password',
+                                ),
+                            ),
+                            volume_mounts=[
+                                k8s_client.V1VolumeMount(
+                                    mount_path='/layout',
+                                    name='layout',
+                                    read_only=True,
+                                ),
+                            ],
+                            volume_devices=[
+                                k8s_client.V1VolumeDevice(
+                                    device_path='/disk',
+                                    name='disk',
+                                ),
+                            ],
+                        ),
+                    ],
+                    volumes=[
+                        k8s_client.V1Volume(
+                            name='disk',
+                            persistent_volume_claim=(
+                                k8s_client.V1PersistentVolumeClaimVolumeSource(
+                                    claim_name=pvc.metadata.name,
+                                )
+                            ),
+                        ),
+                        k8s_client.V1Volume(
+                            name='layout',
+                            config_map=k8s_client.V1ConfigMapVolumeSource(
+                                name=cm.metadata.name,
+                            ),
+                        ),
+                    ],
+                ),
+            ),
+        ),
+    ))
+    logger.info("Created job %s", job.metadata.name)
