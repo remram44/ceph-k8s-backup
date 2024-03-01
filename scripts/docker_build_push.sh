627607aa0f82d918be16cab977df442378fe664b
Remi Rampin <remi@rampin.org>
2024-03-01 13:12:57 -0500

    Add external caching to the build script

--- scripts/docker_build_push.sh
+++ scripts/docker_build_push.sh
@@ -7,6 +7,7 @@ IMAGE=ghcr.io/remram44/ceph-k8s-backup:$VERSION
 
 docker buildx build --pull \
     . \
+    --cache-from type=registry,ref=ghcr.io/remram44/ceph-k8s-backup/buildxcache --cache-to type=registry,ref=ghcr.io/remram44/ceph-k8s-backup/buildxcache,oci-mediatypes=false \
     --platform linux/amd64,linux/arm/v7,linux/arm64 \
     --push --tag $IMAGE
 
